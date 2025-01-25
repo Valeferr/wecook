064b33bddad1d6f6fb7d92eca5a52f63614ec946
Simone Francesco Albino <simonefrancesco.albino@gmail.com>
2025-01-25 14:58:33 +0100

    Added localhost CORS headers configuration

--- server/src/main/java/com/wecook/Main.java
+++ server/src/main/java/com/wecook/Main.java
@@ -1,5 +1,6 @@
 package com.wecook;
 
+import com.wecook.rest.filters.CORSFilter;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -10,8 +11,12 @@ public class Main {
     public static final String BASE_URI = "http://localhost:8080/wecook/";
 
     public static void main(String[] args) {
-        ResourceConfig config = new ResourceConfig().packages("com.wecook.rest");
+        ResourceConfig config = new ResourceConfig()
+                .packages("com.wecook.rest")
+                .register(CORSFilter.class);
+
         HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+
         Runtime.getRuntime().addShutdownHook(new Thread(httpServer::shutdownNow));
     }
 }
---
+++ server/src/main/java/com/wecook/rest/filters/CORSFilter.java
@@ -0,0 +1,19 @@
+package com.wecook.rest.filters;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    }
+}
