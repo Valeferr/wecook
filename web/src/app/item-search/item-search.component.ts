1d5e78d038a898891b1430051eed1d16cd471de0
Valentina Ferrentino <v.ferrentino10@studenti.unisa.it>
2025-02-05 22:19:31 +0100

    Added search page

---
+++ web/src/app/item-search/item-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-search.component.html',
+  styleUrl: './item-search.component.css'
+})
+export class ItemSearchComponent {
+
+}
